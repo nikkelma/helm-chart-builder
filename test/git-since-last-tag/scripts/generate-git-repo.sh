b1028871c0f9b71b9b0b51a728ccade112cf7771
Matt Nikkel <matt.nikkel@harness.io>
2022-07-12 18:02:24 -0700

    Update git generate script for config, fix resets

--- test/git-since-last-tag/scripts/generate-git-repo.sh
+++ test/git-since-last-tag/scripts/generate-git-repo.sh
@@ -5,14 +5,20 @@ cd "${HCB_TEST_BASE_DIR}" || {
   exit 1
 }
 
+git config --global user.email "helm-chart-builder@localhost"
+git config --global user.name "Helm Chart Builder"
+
 git init
 git branch -m main
 
 git add README.md
 git commit -m "Initial commit"
 
+initial_commit_id="$(git rev-parse HEAD)"
+
 # test 1
 git branch main-test-1
+git switch main-test-1
 
 git add charts/nginx-test-a/1.0.0
 git commit -m "Test 1 stage 1: Add chart A v1.0.0"
@@ -32,7 +38,8 @@ git tag test-1-4
 
 # test 2
 git branch main-test-2
-git reset --mixed main
+git switch main-test-2
+git reset "${initial_commit_id}"
 
 git add charts/nginx-test-a/
 git commit -m "Test 2 stage 1: Add chart A v1.0.0 + v1.0.1"
@@ -48,7 +55,8 @@ git tag test-2-3
 
 # test 3
 git branch main-test-3
-git reset --mixed main
+git switch main-test-3
+git reset "${initial_commit_id}"
 
 git add charts/nginx-test-a/
 git commit -m "Test 3 stage 1: Add chart A v1.0.0 + v1.0.1"
@@ -60,7 +68,8 @@ git tag test-3-2
 
 # test 4
 git branch main-test-4
-git reset --mixed main
+git switch main-test-4
+git reset "${initial_commit_id}"
 
 git add charts/nginx-test-a/1.0.0
 git commit -m "Test 4 stage 1: Add chart A v1.0.0"
